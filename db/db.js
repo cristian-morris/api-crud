d45898aafbe9ea8998bf1b1bc81804ec39e3e437
Christian Navarro <christianyahirnavarrochan@gmail.com>
2024-04-16 19:23:34 -0600

    cambios

--- db/db.js
+++ db/db.js
@@ -1,11 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "bvqs08tvbnsw9ov0fv5p-mysql.services.clever-cloud.com",
-    user: "u5q74p3qoa8xv7wh",
-    password: "gDszhr1jlPKoni6uOXr5",
-    database: "bvqs08tvbnsw9ov0fv5p",
-    port: 3306 
+
 });
 
 db.connect((err) => {
