37d8ab489b365cc071ac3229be302db024ba1bd3
Christian Navarro <christianyahirnavarrochan@gmail.com>
2024-04-17 00:01:05 -0600

    cambios

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
+    host: process.env.HOST, 
+    user: process.env.USER, 
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
